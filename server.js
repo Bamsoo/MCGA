31a8e1c5365a8f54be6024b357e1fa1c9dd1c096
François <app.bamsoo@gmail.com>
2025-11-20 01:44:34 +0100

    '---'

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+const puppeteer = require('puppeteer'); 
 const http = require('http');
-
 const name = 'node-hello-world';
 const port = '8888';
-
 const app = new http.Server();
 
 app.on('request', (req, res) => {
+  get();
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('Hello World');
   res.end('\n');
@@ -14,3 +14,12 @@ app.on('request', (req, res) => {
 app.listen(port, () => {
   console.log(`${name} is listening on port ${port}`);
 });
+
+
+const get = async function(a, b) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://focus-pardot.fr/form/');
+  const pageTitle = await page.title();
+  await browser.close();
+}
