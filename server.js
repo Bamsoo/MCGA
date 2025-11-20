485e6c7e9e3c6fcb0f44d0b3a9e51e9e4091f13e
François <app.bamsoo@gmail.com>
2025-11-20 22:42:05 +0100

    '---'

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ app.listen(port, () => {
 
 
 const get = async function(a, b) {
-  //const browser = await puppeteer.launch({executablePath: '/path/to/Chrome'});
   const browser = await puppeteer.launch({args: ['--no-sandbox'], executablePath: '/opt/render/project/src/chrome/linux-142.0.7444.175/chrome-linux64/chrome'});
   const page = await browser.newPage();
-  await page.goto('https://focus-pardot.fr/form/');
+  await page.goto('https://m7g-cdn.bamsoo.com/lp/landing-page/fake-rt-landing-page-MCY5VPXXPYA5GOZFPJUKUCEZRTVQ?firstName=aaa&lastName=bbb&company=ccc&email=fp@bamsoo.com');
   const pageTitle = await page.title();
   console.log('Page title: ' + pageTitle);
   await browser.close();
