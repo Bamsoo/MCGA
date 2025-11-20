0707bbbe097511d38a132624b24f4f13d59981fb
François <app.bamsoo@gmail.com>
2025-11-20 02:01:24 +0100

    '---'

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.listen(port, () => {
 
 
 const get = async function(a, b) {
+  //const browser = await puppeteer.launch({executablePath: '/path/to/Chrome'});
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://focus-pardot.fr/form/');
